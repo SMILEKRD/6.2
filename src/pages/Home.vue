6008b6147475b74b0d22add4fdb46952a3a0931e
SMILEKRD <112694026+SMILEKRD@users.noreply.github.com>
2022-10-08 09:51:42 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <Navbar />
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+export defaulf {
+    components: { Navbar }
+    <div class="content">
+        <h3>hi, i am Kritlada</h3>
+        <span>"Sleep More"</span>
+        <p>I'm 19 year old, Now I'm study at Bangkok University Multimedia and Entertainment Engineering</p>
+        <a href="about.html" class="btn"> about me <i class="fas fa-user"></i> </a>
+    </div>
+}
+</script>
+
+<style>
+
+</style>
